b0d7915a9e8cc7fa92de31327603720b6a167d3c
Lee Kah Seng <kahseng.lee123@gmail.com>
2020-04-11 16:53:23 +0800

    Created project

---
+++ SwiftSenpai-Lottie-Advance-Playback/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSenpai-Lottie-Advance-Playback
+//
+//  Created by Lee Kah Seng on 11/04/2020.
+//  Copyright © 2020 Lee Kah Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
